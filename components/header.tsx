e45b34fad89c937cfb7526683fcd88bbed9f58f8
Benja-san <beugnet.freelance@gmail.com>
2023-03-10 12:13:07 +0100

    Add sidebar for layout

--- components/header.tsx
+++ components/header.tsx
@@ -9,10 +9,25 @@ const bigJohnPro = localFont({
     fallback: ['system-ui', 'arial', 'sans-serif']
 })
 
-export default function Header() {
+export default function Header({setActiveMenu} : any) {
+
+    function toggleMenu() {
+        setActiveMenu(true)
+    }
+
     return <header className={styles.header}>
         <Link href="/" legacyBehavior>
             <a className={`${bigJohnPro.variable} ${styles.logo}`}>Sk<span>i</span>lls R<span>o</span>om</a>
         </Link>
+        <div className={styles.menu}>
+            <button onClick={toggleMenu} className={styles.burger} type="button">
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="8" height="8" fill="#FFFDFA"/>
+                    <rect y="10" width="8" height="8" fill="#FFFDFA"/>
+                    <rect x="10" y="10" width="8" height="8" fill="#FFFDFA"/>
+                    <rect x="10" width="8" height="8" fill="#FFFDFA"/>
+                </svg>
+            </button>
+        </div>
     </header>
 }
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,21 @@
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useState } from "react"
 import Header from "./header"
 import Footer from "./footer"
+import Sidebar from "./sidebar"
 import styles from '@/styles/Layout.module.scss'
 
 export default function Layout({ children }: PropsWithChildren ) {
+
+    const [activeMenu, setActiveMenu] = useState<boolean>(false)
+
     return <>
-        <Header/>
+        <Header
+            setActiveMenu={setActiveMenu}
+        />
+        <Sidebar
+            setActiveMenu={setActiveMenu}
+            activeMenu={activeMenu}
+        />
         <main className={styles.main} >
             {children}
         </main>
