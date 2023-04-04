e3227b397fd2bb7b8171171a4723a34559400e59
Benja-san <beugnet.freelance@gmail.com>
2023-04-04 16:00:48 +0200

    add effect on sidebar

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -18,46 +18,47 @@ const Sidebar: FunctionComponent<Props> = ({ activeMenu, onClick, font}) => {
   console.log(t("sidebar", { returnObjects: true, ns: "navigation" }))
 
   return (
-    <nav
-      className={`${styles.navigation} ${activeMenu ? styles.displayed : ""}`}
-    >
-      <div className={`${styles.menu}`}>
-        <button
-          onClick={onClick}
-          className={`${styles.burger} ${styles.active}`}
-          type="button"
-        >
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+    <div className={`${styles.navigationContainer} ${activeMenu ? styles.displayed : ""}`}>
+      <div onClick={onClick} className={styles.notNavigation}></div>
+      <nav className={styles.navigation} >
+        <div className={`${styles.menu}`}>
+          <button
+            onClick={onClick}
+            className={`${styles.burger} ${styles.active}`}
+            type="button"
           >
-            <rect width="8" height="8" fill="#FFFDFA" />
-            <rect y="10" width="8" height="8" fill="#FFFDFA" />
-            <rect x="10" y="10" width="8" height="8" fill="#FFFDFA" />
-            <rect x="10" width="8" height="8" fill="#FFFDFA" />
-          </svg>
-        </button>
-      </div>
-      <ul className={`${styles.linksContainer} ${font.className}`}>
-        {Object.values(t("sidebar", { returnObjects: true, ns:"navigation" })).map(
-          (section, key) => {
-            return (
-              <li key={key+1} className={styles.linkContainer}>
-                <Link href={section.url}>
-                  <div className={styles.link}>
-                    <SVGIcons type={section.icon} />
-                    <ActionLink title={section.title} />
-                  </div>
-                </Link>
-              </li>
-            )
-          }
-        )}
-      </ul>
-    </nav>
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="8" height="8" fill="#FFFDFA" />
+              <rect y="10" width="8" height="8" fill="#FFFDFA" />
+              <rect x="10" y="10" width="8" height="8" fill="#FFFDFA" />
+              <rect x="10" width="8" height="8" fill="#FFFDFA" />
+            </svg>
+          </button>
+        </div>
+        <ul className={`${styles.linksContainer} ${font.className}`}>
+          {Object.values(t("sidebar", { returnObjects: true, ns:"navigation" })).map(
+            (section, key) => {
+              return (
+                <li onClick={onClick} key={key+1} className={styles.linkContainer}>
+                  <Link href={section.url}>
+                    <div className={styles.link}>
+                      <SVGIcons type={section.icon} />
+                      <ActionLink title={section.title} />
+                    </div>
+                  </Link>
+                </li>
+              )
+            }
+          )}
+        </ul>
+      </nav>
+    </div>
   )
 }
 
