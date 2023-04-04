053264b72ab7e728b581d850ce54a086843c589f
Benja-san <beugnet.freelance@gmail.com>
2023-04-04 18:06:19 +0200

    add light bubble

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/layout/Sidebar.module.scss"
 import { NextFontWithVariable } from "next/dist/compiled/@next/font"
 import Link from "next/link"
 import {useRouter} from 'next/router';
-import { FunctionComponent } from "react"
+import { FunctionComponent, useState } from "react"
 import SVGIcons from "../global/SVGIcons"
 import ActionLink from "../global/ActionLink"
 
@@ -18,31 +18,62 @@ const Sidebar: FunctionComponent<Props> = ({ activeMenu, onClick, font}) => {
   const { t } = useTranslation("navigation")
   const router = useRouter()
   let currentUrl = router.pathname
+  const currentLocale = router.locale
+  const [isLanguageBoxDisplayed, setIsLanguageBoxDisplayed] = useState(false)
+  function toggleLanguageBox() {
+    setIsLanguageBoxDisplayed(!isLanguageBoxDisplayed)
+  }
 
   return (
     <div className={`${styles.navigationContainer} ${activeMenu ? styles.displayed : ""}`}>
       <div onClick={onClick} className={styles.notNavigation}></div>
-      <nav className={styles.navigation} >
-        <div className={`${styles.menu}`}>
-          <button
-            onClick={onClick}
-            className={`${styles.burger} ${styles.active}`}
-            type="button"
-          >
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+      <nav className={`${styles.navigation} ${font.className}`} >
+        <ul className={`${styles.menu}`}>
+          <li className={styles.menuList}>
+            <button className={styles.theme} type="button">
+              <svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 0L9.75 6.75H16.5L15.1005 8.25H4.3005L12 0ZM13.7002 9.75H2.90025L1.5 11.25H8.25L6 18L13.7002 9.75Z" fill="#FFFDFA"/>
+              </svg>
+            </button>
+          </li>
+          <li className={styles.menuList}>
+            <button 
+              type="button" 
+              className={`${font.className} ${styles.language} ${isLanguageBoxDisplayed && styles.active}`}
+              onClick={toggleLanguageBox}
             >
-              <rect width="8" height="8" fill="#FFFDFA" />
-              <rect y="10" width="8" height="8" fill="#FFFDFA" />
-              <rect x="10" y="10" width="8" height="8" fill="#FFFDFA" />
-              <rect x="10" width="8" height="8" fill="#FFFDFA" />
-            </svg>
-          </button>
-        </div>
+              {currentLocale}
+            </button>
+            <ul className={`${styles.languagesContainer} ${isLanguageBoxDisplayed && styles.displayed}`}>
+              <li className={styles.languageList}>
+                <a href={`/en${currentUrl}`}>en</a>
+              </li>
+              <li className={styles.languageList}>
+                <a href={`/fr${currentUrl}`}>fr</a>
+              </li>
+            </ul>
+          </li>
+          <li className={styles.menuList}>
+            <button
+              onClick={onClick}
+              className={`${styles.burger} ${styles.active}`}
+              type="button"
+            >
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="8" height="8" fill="#FFFDFA" />
+                <rect y="10" width="8" height="8" fill="#FFFDFA" />
+                <rect x="10" y="10" width="8" height="8" fill="#FFFDFA" />
+                <rect x="10" width="8" height="8" fill="#FFFDFA" />
+              </svg>
+            </button>
+          </li>
+        </ul>
         <ul className={`${styles.linksContainer} ${font.className}`}>
           {Object.values(t("sidebar", { returnObjects: true })).map(
             (section, key) => {
